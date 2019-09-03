c1a85dcd071a19853438d1eedca3d64f40eb8dc0
Nicke <Nicholas0012@gmail.com>
2019-09-03 11:05:05 +0200

    first input

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ let fakeDataBase = [
     {id:1, task: "Gå och slå måns", ready:false}
 ];
 
-
 renderFakeData();
 
 function renderFakeData(){
@@ -19,7 +18,29 @@ function renderFakeData(){
 
     getId("taskList").innerHTML = htmlOutput.join("");
 
-    function getId(id){
-        return document.getElementById(id);
-    }
+   
+}
+
+function getId(id){
+    return document.getElementById(id);
+}
+
+//Lyssna efter form-submit.
+getId("taskForm").addEventListener("submit", addTask);
+
+function addTask(event){
+    //hindra formuläret till servern
+    event.preventDefault();
+    //hämta input datan
+    let input = getId("taskId").value;
+    //skapa ett taskobject
+    let taskObject = {task:input, ready:false}
+    //spara i fakeDataBase
+    fakeDataBase.push(taskObject);
+    //renderar nytt
+    renderFakeData();
+
+    getId("taskId").value = "";
+    getId("taskId").focus();
+
 }
